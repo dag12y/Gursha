7fe8c9dc83b2fb6ad112ea9b1fbd2ff98d6f7eaa
dagm <dagimyibabe19@gmail.com>
2026-02-10 14:03:51 +0300

    feat: implement database connection setup with mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import connectDB from './config/db.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/Gursha';
 const app = express();
 
+//connect to database
+connectDB(MONGO_URI);
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
